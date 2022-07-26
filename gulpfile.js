c1a7cb3cc88ae3b7759901708e172db5fda96f7a
Josué Alarcón <josuealarco@gmail.com>
2022-07-26 18:55:48 -0500

    Adding Webp Images

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@ const gulp= require('gulp');
 const {series, parallel}  = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-
+const webp = require('gulp-webp');
 function buildStyles() {
     return gulp.src('./src/sass/**/*.scss')
       .pipe(sourcemaps.init())
       .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./build/css'));
-  };
-  
+};
+
+function convertWebp() {
+    return gulp.src('./src/images/**/*')
+          .pipe(webp())
+          .pipe(gulp.dest('./build/images'));
+}
 function watchers () {
     gulp.watch('./src/sass/**/*.scss', buildStyles);
+    gulp.watch('./src/images/**/*', convertWebp)
+
 }
 exports.buildStyles = buildStyles;
-exports.default = parallel(watchers);
+exports.default = parallel(buildStyles, convertWebp,watchers);
